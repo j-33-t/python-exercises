42c00e2ce1a4c5523a5c4639e5c355a94a7dd877
j-33-t <prasenjeet.rathore@gmail.com>
2022-04-21 12:28:52 +0200

    21-04-2022-3rd commit

--- Basic-part-1.py
+++ Basic-part-1.py
@@ -5,7 +5,9 @@
 
 # 1. Write a Python program to get the Python version you are using
 
+from logging import raiseExceptions
 from platform import python_version
+
 
 
 
@@ -21,7 +23,7 @@ print(f"today's date is: {Today}")
 
 
 # 3. Write a Python program which accepts the radius of a circle from the user and compute the area
-from math import pi
+from math import floor, pi
 
 r = float(input("Enter radius of the circle: "))
 area = pi*r**2
@@ -68,3 +70,193 @@ print(exam_st_date)
 # Solution 2
 exam_st_date = (11, 12, 2014)
 print("Exam date will start from : %i / %i / %i " %exam_st_date)
+
+# 9. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+n = int(input("Enter a number: "))
+
+n1 = int("%i" %n)
+n2 = int("%i%i" %(n,n))
+n3 = int("%i%i%i" %(n,n,n))
+print(f"{n1+n2+n3}")
+
+# 10 . Write a Python program to print the docstring  of Python built-in function(s).
+
+print(f"{int().__doc__}")
+
+# 11. Write a Python program to print the calendar of a given month and year.
+import calendar
+y = 2022
+m = 4
+print(calendar.month(y,m))
+
+# 12.  Write a Python program to print the following
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+
+print("""a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example""")
+
+
+# 13. Write a Python program to calculate number of days between two dates.
+# Sample : (2014, 7, 2), (2014, 7, 11)
+
+date1 = (2014, 7, 2)
+date2 = (2014, 7, 11)
+
+date1 = str(date1) \
+    .replace("(","") \
+        .replace(")","")
+        
+
+date2 = str(date2) \
+    .replace("(","") \
+        .replace(")","")
+        
+date1 = datetime.strptime(date1, "%Y, %m, %d")
+date2 = datetime.strptime(date2, "%Y, %m, %d")
+
+diff = (date2 - date1).days
+print(f"difference in days: {diff}")
+
+# 14.  Write a Python program to get the volume of a sphere with radius 6.
+# formua = 4/3 pi*r**2
+r = 6
+area_of_sphere = (4/3)*pi*(r**3)
+print(f"are of sphere is : {round(area_of_sphere, 2)}")
+
+# 15. Write a Python program to get the difference between a given number and 17, 
+# if the number is greater than 17 return double the absolute difference
+
+def difference(n):
+    if n > 17:
+        print((n-17)*2)
+    else: 
+        print(17 - n )
+        
+n = int(input("Enter number: "))
+difference(n)
+
+# 16. Write a Python program to test whether a number is within range 100 to 1000 or 2000.
+
+def test(n):
+    if n < 100:
+        print("Less than 100")
+    elif n < 1000:
+        print("between 100 and 1000")
+    elif n < 2000:
+        print("between 1000 and 2000")
+    else:
+        print("n greater than 2000")
+        
+n = int(input("Enter a number: "))
+test(n)
+
+# 17. Write a Python program to calculate the sum of three given numbers, 
+# if the values are equal then return three times of their sum.
+
+def three_times(n,n1,n2):
+    if n == n1 == n2:
+        print(f"{(n+n1+n2)*3}")
+    else:
+        print(f"{n+n1+n2}")
+        
+three_times(1,2,3)
+three_times(3,3,3)
+
+# 18.  Write a Python program to get a new string from a given string where "Is" has been added to the front. 
+# If the given string already begins with "Is" then return the string unchanged
+
+def is_start(string):
+    if string.startswith("Is"):
+        print(string)
+    else:
+        print(f"Is {string}")
+    
+sentance = str(input("Enter a sentance: "))
+is_start(sentance)
+
+# 19. Write a Python program to find whether a given number (accept from the user) is even or odd, 
+# print out an appropriate message to the user
+
+def odd_even(n):
+    if (n%2) == 0:
+        print(f"{n} is an even number")
+    else:
+        print(f"{n} is an odd number")
+
+odd_even(20)
+odd_even(11)
+
+# 20. Write a Python program to count the number 4 in a given list
+n = [1,2,3,4,5,6,5,4,3,2,1,4]
+
+def count_4(array):
+    count = 0
+    for n in array:
+        if n == 4:
+            count = count +1
+    return count
+
+count_4(n)
+
+# 21. Write a Python program to get a string which is n (non-negative integer) copies of a given string.
+
+def n_string(string,n):
+    text = ""
+    for i in range(n):
+        text = text + string
+    return text
+
+print(n_string("Messi ", 3))
+
+
+# 22. Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string. 
+# Return the n copies of the whole string if the length is less than 2.
+
+def n_2_string(string,n):
+    text = ""
+    for i in range(n):
+        if len(string) < 2:
+            text = text + string
+        else:
+            text = text + string[0] + string[1]
+    return text
+            
+n_2_string("messi", 3)
+
+# 23. Write a Python program to test whether a passed letter is a vowel or not.
+
+
+def vowel_check(string):
+    # Converting input to string and to lowercase
+    string = str(string).lower()
+    
+    # Checking if string is of length == 1
+    if len(string) > 1:
+        return TypeError("Only input single character")
+    else:
+        pass
+    
+    # Checking if string contains only alphabets
+    if string.isalpha():
+        pass
+    else:
+        return TypeError("Only alphabets allowed as input")
+    
+    # Storing all vowels for further conditional statements    
+    vowels = ["a","e","i","o","u"]
+
+    # Checking if entered character is a vowel or not
+    if string in vowels:
+        print("Entered letter is a vowel")
+    else:
+        print("Entered letter is not a vowel")
+
+
+
+vowel_check("a")
+vowel_check(1)
